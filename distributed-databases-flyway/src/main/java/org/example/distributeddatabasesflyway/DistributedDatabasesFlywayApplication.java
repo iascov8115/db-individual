86d9968e4118b1cc8baef1d58ed9eabaec2b47dc
vitali.caraivanov <vitaly.caraivanov@gmail.com>
2025-11-13 22:14:26 +0200

    Initial commit for flyway runner module

---
+++ distributed-databases-flyway/src/main/java/org/example/distributeddatabasesflyway/DistributedDatabasesFlywayApplication.java
@@ -0,0 +1,13 @@
+package org.example.distributeddatabasesflyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedDatabasesFlywayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedDatabasesFlywayApplication.class, args);
+    }
+
+}
---
+++ distributed-databases-flyway/src/test/java/org/example/distributeddatabasesflyway/DistributedDatabasesFlywayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.distributeddatabasesflyway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedDatabasesFlywayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
